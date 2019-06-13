ba63b14517d9688b82a237b53e50601a0cc42350
hollandjb <hollandjb@vcu.edu>
2019-06-12 20:02:47 -0400

    added background image, finalized css and html, tried to add download button but download attribute has browser specific issue

--- index.js
+++ index.js
@@ -12,6 +12,7 @@ function intiialButtons() {
   })
 }
 
+//add another pokemon button
 function addPokemon() {
   $("#addPokemon").on("click", function (event) {
     event.preventDefault();
@@ -25,40 +26,42 @@ function addPokemon() {
   });
 }
 
-
+//deletes the user added buttons
 $("body").on("click", "#delete", function () {
   intiialButtons();
 })
 
-
-// intiialButtons();
-
-
-//
+//generates gifs upon clicking pokemon button
 $("body").on("click", ".pokemonButton", function () {
+  //stores pokemon name is a variable
   let pokemon = $(this).attr("data-pokemon");
+  //concatenates query URL
   let queryURL = "https://api.giphy.com/v1/gifs/search?q=" +
     pokemon + "&api_key=4flbhHzwF8RVAQ04WmYoFBcfQ9LjhObv&limit=10&rating=PG-13";
+  //call ajax method
   $.ajax({
     method: "GET",
     url: queryURL
   }).then(function (response) {
     console.log(response);
     let results = response.data
-
     for (i = 0; i < results.length; i++) {
       let pokemonDiv = $("<div>");
-      let p = $("<h3>").html("Rating: <strong>" + results[i].rating.toUpperCase() + "</strong>");
-      let pokemonGif = $("<img class='gif mb-5' src=" + results[i].images.fixed_height_still.url + ">");
+      let p = $("<h2>").html("Rating: <strong>" + results[i].rating.toUpperCase() + "</strong>");
+      let pokemonGif = $("<img class='gif mb-4' src=" + results[i].images.fixed_height_still.url + ">");
       pokemonGif.attr("data-state", "still").attr("data-animate", results[i].images.fixed_height.url).attr("data-still", results[i].images.fixed_height_still.url);
+      //download attribute doesnt work anymore but hey I tried
+      let download = $("<button>").addClass("btn btn-outline-danger mb-5 d-block download shadow-none").html("<a target='_blank' href='" + results[i].images.fixed_height.url + "' download>Download</a>");
       pokemonDiv.append(p);
       pokemonDiv.append(pokemonGif);
+      pokemonDiv.append(download);
       $("#gifs").prepend(pokemonDiv);
     }
 
   })
 
 });
+
 
 //toggle to pause and play gif
 $("body").on("click", ".gif", function () {
@@ -71,6 +74,8 @@ $("body").on("click", ".gif", function () {
     $(this).attr("data-state", "still");
   }
 });
+
+//
 
 intiialButtons();
 addPokemon();
